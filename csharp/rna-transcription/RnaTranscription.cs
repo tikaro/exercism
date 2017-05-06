194d418395514f195bb3a9f1cf6ee1f139cee129
John Young <john.young@thinkcompany.com>
2017-05-06 10:55:03 -0400

    Create an initial solution

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -4,6 +4,18 @@ public static class Complement
 {
     public static string OfDna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // DNA CYTOSINE to RNA guanine
+        nucleotide = nucleotide.Replace('C', 'g');
+
+        // DNA GUANINE to RNA cytosine;
+        nucleotide = nucleotide.Replace('G', 'c');
+
+        // DNA THYMINE to RNA adenine;
+        nucleotide = nucleotide.Replace('T', 'a');
+
+        // DNA ADENINE to RNA uracil;
+        nucleotide = nucleotide.Replace('A', 'u');
+
+        return nucleotide.ToUpper();
     }
 }
