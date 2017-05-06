5be0b84452058090da68d49e830e7da8d10b2afc
John Young <john.young@thinkcompany.com>
2017-05-06 11:11:55 -0400

    Rewrite the solution using a C#-specific Dictionary feature

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,21 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 public static class Complement
 {
     public static string OfDna(string nucleotide)
     {
-        // DNA CYTOSINE to RNA guanine
-        nucleotide = nucleotide.Replace('C', 'g');
+        Dictionary<char, char> dnaToRnaTranscription = new Dictionary<char, char>
+        {
+            { 'C', 'G' },
+            { 'G', 'C' },
+            { 'T', 'A' },
+            { 'A', 'U' }
+        };
 
-        // DNA GUANINE to RNA cytosine;
-        nucleotide = nucleotide.Replace('G', 'c');
-
-        // DNA THYMINE to RNA adenine;
-        nucleotide = nucleotide.Replace('T', 'a');
-
-        // DNA ADENINE to RNA uracil;
-        nucleotide = nucleotide.Replace('A', 'u');
-
-        return nucleotide.ToUpper();
+        string rnastrand = "";
+        foreach (char letter in nucleotide) {
+            rnastrand += dnaToRnaTranscription[letter].ToString();
+        }
+        return rnastrand;
     }
 }
