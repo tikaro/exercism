2ba35a045cb1f2ac18c2c4ccb306de0b3e19b429
John Young <john.young@thinkcompany.com>
2017-05-07 20:43:01 -0400

    SpaceAge solution

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,47 +2,31 @@ using System;
 
 public class SpaceAge
 {
+    public long Seconds;
+    private const double EarthYear = 31557600D;
+    private const double MercuryYear = EarthYear * 0.2408467D;
+    private const double VenusYear = EarthYear * 0.61519726D;
+    private const double MarsYear = EarthYear * 1.8808158D;
+    private const double JupiterYear = EarthYear * 11.862615D;
+    private const double SaturnYear = EarthYear * 29.447498D;
+    private const double UranusYear = EarthYear * 84.016846D;
+    private const double NeptuneYear = EarthYear * 164.79132D;   
+
     public SpaceAge(long seconds)
     {
+        this.Seconds = seconds;
     }
 
-    public double OnEarth()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnMercury()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnVenus()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnMars()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnJupiter()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnSaturn()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnUranus()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnNeptune()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    public double OnEarth() { return CalcYear(EarthYear); }
+    public double OnMercury() { return CalcYear(MercuryYear); }
+    public double OnVenus() { return CalcYear(VenusYear); }
+    public double OnMars() { return CalcYear(MarsYear); }
+    public double OnJupiter() { return CalcYear(JupiterYear); }
+    public double OnSaturn() { return CalcYear(SaturnYear); }
+    public double OnUranus() { return CalcYear(UranusYear); }
+    public double OnNeptune() { return CalcYear(NeptuneYear); }
+    public double CalcYear(double yearLength) { 
+        double theAge = this.Seconds / yearLength;
+        return Math.Round(theAge, 2);
     }
 }
