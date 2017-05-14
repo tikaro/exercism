f66713dc758f79e90731f7f622f4fe80132a4548
John Young <john.young@thinkcompany.com>
2017-05-13 21:35:22 -0400

    First iteration

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            if (!input.ToUpper().Contains(c)) { return false; }
+        }
+        return true;
     }
 }
