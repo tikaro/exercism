c0539b2d0aa9568a39840a8f355e12346fd31194
John Young <john.young@thinkcompany.com>
2017-05-19 17:36:27 -0400

    Add the Pangram solution

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -5,10 +5,6 @@ public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        for (char c = 'A'; c <= 'Z'; c++)
-        {
-            if (!input.ToUpper().Contains(c)) { return false; }
-        }
-        return true;
+        return "abcdefghijklmnopqrstuvwxyz".All(input.ToLower().Contains);
     }
 }
