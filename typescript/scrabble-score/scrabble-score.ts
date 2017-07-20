a063ee903bf91aad498e3b093ab420ff6caf6c59
John Young <john.young@thinkcompany.com>
2017-07-20 08:22:33 -0400

    Second solution, slightly DRY-er

--- typescript/scrabble-score/scrabble-score.ts
+++ typescript/scrabble-score/scrabble-score.ts
@@ -1,38 +1,25 @@
 export default function score(input: string = ''): number {
     var word: string = input
     var score: number = 0
-    
-    // create dictionary myMap and fill it with the alphabet
+
     const scrabbleMap = new Map<string, number>()
 
-    'aeioulnrst'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 1)
-    })
+    // fill the dictionary with tiles and their corresponding scores
+    function addtiles(tiles: string, score: number) {
+        tiles.split('').forEach((key: string) => {
+            scrabbleMap.set(key.toLowerCase(), score)
+        })
+    }
 
-    'dg'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 2)
-    })
+    addtiles('aeioulnrst', 1)
+    addtiles('dg', 2)
+    addtiles('bcmp', 3)
+    addtiles('fhvwy', 4)
+    addtiles('k', 5)
+    addtiles('jx', 8)
+    addtiles('qz', 10)
 
-    'bcmp'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 3)
-    })
-
-    'fhvwy'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 4)
-    })
-
-    'k'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 5)
-    })
-
-    'jx'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 8)
-    })
-
-    'qz'.split('').forEach((key: string) => {
-        scrabbleMap.set(key.toLowerCase(), 10)
-    })
-
+    // look up each tile in the dictionary and add it to the score
     for (const each of word) {
         const value = scrabbleMap.get(each.toLowerCase()) || 0
         score += value
