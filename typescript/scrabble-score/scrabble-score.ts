614f555a49c944f3412469812caf851e66ebf107
John Young <john.young@thinkcompany.com>
2017-07-19 20:30:01 -0400

    First solution for scrabble-score

--- typescript/scrabble-score/scrabble-score.ts
+++ typescript/scrabble-score/scrabble-score.ts
@@ -1,12 +1,42 @@
-export default class ScrabbleScore {
-    score: number
+export default function score(input: string = ''): number {
+    var word: string = input
+    var score: number = 0
+    
+    // create dictionary myMap and fill it with the alphabet
+    const scrabbleMap = new Map<string, number>()
 
-    costructor(input: string) {
-        this.score = this._score(input)
+    'aeioulnrst'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 1)
+    })
+
+    'dg'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 2)
+    })
+
+    'bcmp'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 3)
+    })
+
+    'fhvwy'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 4)
+    })
+
+    'k'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 5)
+    })
+
+    'jx'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 8)
+    })
+
+    'qz'.split('').forEach((key: string) => {
+        scrabbleMap.set(key.toLowerCase(), 10)
+    })
+
+    for (const each of word) {
+        const value = scrabbleMap.get(each.toLowerCase()) || 0
+        score += value
     }
 
-    private _score(word: string): number {
-        word = "hi" // do something with this to stop the red squiggly
-        return 0    // to get the first test to pass
-    }
+    return score
 }
