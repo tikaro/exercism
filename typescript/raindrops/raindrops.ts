8173f4b2af484fef4a77461ea9030b53ed2f733f
John Young <john.young@thinkcompany.com>
2017-07-24 20:05:55 -0400

    First solution

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,5 +1,23 @@
 export default class Raindrops {
   convert(input: number): string {
-    return input.toString()
+    let response: string = ""
+
+    if (input % 3 == 0) {
+      response += "Pling"
+    }
+
+    if (input % 5 == 0) {
+      response += "Plang"
+    }
+
+    if (input % 7 == 0) {
+      response += "Plong"
+    }
+
+    if (response.length == 0) {
+      response = input.toString()
+    }
+
+    return response
   }
 }
