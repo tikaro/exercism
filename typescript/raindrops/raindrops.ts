d2178f56bb610387e5daa6be56600b68945aa99c
John Young <john.young@thinkcompany.com>
2017-07-24 20:13:34 -0400

    Bring if statements onto one line to tighten the syntax

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -2,21 +2,10 @@ export default class Raindrops {
   convert(input: number): string {
     let response: string = ""
 
-    if (input % 3 == 0) {
-      response += "Pling"
-    }
-
-    if (input % 5 == 0) {
-      response += "Plang"
-    }
-
-    if (input % 7 == 0) {
-      response += "Plong"
-    }
-
-    if (response.length == 0) {
-      response = input.toString()
-    }
+    if (input % 3 === 0) { response += "Pling" }
+    if (input % 5 === 0) { response += "Plang" }
+    if (input % 7 === 0) { response += "Plong" }
+    if (response.length === 0) { response = input.toString() }
 
     return response
   }
