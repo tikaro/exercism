c14f64876aa6a4322aa285d6bde9e713199c4a21
John Young <john.young@thinkcompany.com>
2017-07-25 08:34:46 -0400

    Third solution. I'm happy with this one

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,28 +1,17 @@
 export default class Bob {
   hey(phrase: string): string {
 
-    // Respond to silence
-    if (phrase.trim().length === 0 ) {
-      return 'Fine. Be that way!'
-    }
+    // Silence
+    if (phrase.trim().length === 0) { return 'Fine. Be that way!' }
 
-    // Respond to ALL CAPS
+    //All caps
     if ( !(phrase.toLowerCase() === phrase) &&
           (phrase.toUpperCase() === phrase)) {
             return "Whoa, chill out!"
     }
 
-    // Respond to a question
-    if (phrase[phrase.length - 1] === '?') {
-      return 'Sure.'
-    }
-
-    // Respond to a forceful statement
-    if (phrase[phrase.length - 1] === '!') {
-      return 'Whatever.'
-    }
-
-    // Everything else
-    return 'Whatever.'
+    if (phrase.endsWith('?')) { return 'Sure.' } // Question
+    if (phrase.endsWith('!')) { return 'Whatever.' } // Shouting
+    return 'Whatever.' // everything else
   }
 }
