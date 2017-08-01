29896790bb25d150f2607b8a0f1da0f22717995f
John Young <john.young@thinkcompany.com>
2017-08-01 08:09:30 -0400

    Initial commit of the phone-number exercise

---
+++ typescript/word-count/word-count-bravo.ts
@@ -0,0 +1,17 @@
+export default class Words {
+  count(input: string): Map<string, number> {
+    const inputArray = input
+                        .replace(/\t/, " ")
+                        .replace(/\n/, " ")
+                        .toLowerCase()
+                        .split(" ")
+    const inputMap = new Map<string, number>()
+
+    for (const each of inputArray) {
+      if (each === "") {continue}
+      const value = inputMap.get(each) || 0
+      inputMap.set(each.trim(), value + 1)
+    }
+    return inputMap
+  }
+}
