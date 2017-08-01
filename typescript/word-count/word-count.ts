82212afd11231e1b5492031ad17b2297f4689b35
John Young <john.young@thinkcompany.com>
2017-07-31 21:33:50 -0400

    Updated with non-cribbed solution

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,12 +1,16 @@
 export default class Words {
   count(input: string): Map<string, number> {
-    const inputArray = input.toLowerCase().replace(/\n/m, " ").replace(/\t/m, " ").split(" ")
+    const inputArray = input
+                        .replace(/\n/, " ")
+                        .replace(/\t/, " ")
+                        .toLowerCase()
+                        .split(" ")
+
     const inputMap = new Map<string, number>()
 
     for (const each of inputArray) {
       if (each === "") {continue}
       const value = inputMap.get(each) || 0
-
       inputMap.set(each.trim(), value + 1)
     }
     return inputMap
