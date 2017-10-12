028781f5e1dfe353c681073f8bf3a2aa5b0c22d1
John Young <john@tikaro.net>
2017-10-12 17:53:58 -0400

    Solution to prime factors

--- typescript/prime-factors/prime-factors.test.ts
+++ typescript/prime-factors/prime-factors.test.ts
@@ -11,15 +11,15 @@ describe('calculatePrimeFactors', () => {
 
   it('factors 6', () => expect(calculatePrimeFactors(6)).toEqual([2, 3]))
 
-  xit('factors 8', () => expect(calculatePrimeFactors(8)).toEqual([2, 2, 2]))
+  it('factors 8', () => expect(calculatePrimeFactors(8)).toEqual([2, 2, 2]))
 
-  xit('factors 9', () => expect(calculatePrimeFactors(9)).toEqual([3, 3]))
+  it('factors 9', () => expect(calculatePrimeFactors(9)).toEqual([3, 3]))
 
-  xit('factors 27', () => expect(calculatePrimeFactors(27)).toEqual([3, 3, 3]))
+  it('factors 27', () => expect(calculatePrimeFactors(27)).toEqual([3, 3, 3]))
 
-  xit('factors 625', () => expect(calculatePrimeFactors(625)).toEqual([5, 5, 5, 5]))
+  it('factors 625', () => expect(calculatePrimeFactors(625)).toEqual([5, 5, 5, 5]))
 
-  xit('factors 901255', () => expect(calculatePrimeFactors(901255)).toEqual([5, 17, 23, 461]))
+  it('factors 901255', () => expect(calculatePrimeFactors(901255)).toEqual([5, 17, 23, 461]))
 
-  xit('factors 93819012551', () => expect(calculatePrimeFactors(93819012551)).toEqual([11, 9539, 894119]))
+  it('factors 93819012551', () => expect(calculatePrimeFactors(93819012551)).toEqual([11, 9539, 894119]))
 })
--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,21 +1,14 @@
-export default function calculatePrimeFactors(input: number) {
-  let factorArray: any[] = []
-
-  //First pass through. Iterate from 2 up, recording factors.
-  for( let i:number = 2; i <= input; i++) {
-    if (isPrime(i) && input % i === 0) { 
-      factorArray.push(i) // add the first factor to the result
-      if(i < input) { factorArray.push(input / i) } // add the second factor to the result if it is less than the input
+export default function calculatePrimeFactors (input: number): number[] {
+  const factors = []
+  let currentFactor = 2
+  while (input !== 1) {
+    if ( input % currentFactor === 0 ) {
+      factors.push(currentFactor)
+      input /= currentFactor
+      currentFactor = 2
+    } else {
+      currentFactor ++
     }
   }
-  
-  console.log( `input is ${input}. factorArray is ${factorArray}` )
-  return factorArray
-}
-
-function isPrime(input: number) {
-  for( let i:number = 2; i < input; i++) {
-    if( input % i === 0) { return false }
-  }
-  return true
+  return factors
 }
