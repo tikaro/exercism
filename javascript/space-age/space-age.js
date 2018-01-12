d6b1f6d7bb0713c69cee873f1e5fcecedf5ab5d1
John Young <john.young@thinkcompany.com>
2018-01-12 08:17:55 -0500

    First version with all tests passing

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -2,29 +2,50 @@
 
 var SpaceAge = function(seconds) {
   this.seconds = seconds;
-  this.earthYear = 31557600;
-  this.mercuryYear = this.earthYear * 0.2408467;
+  this.earthYear    = 31557600;
+  this.mercuryYear  = this.earthYear * 0.2408467;
+  this.venusYear    = this.earthYear * 0.61519726;
+  this.marsYear     = this.earthYear * 1.8808158;
+  this.jupiterYear  = this.earthYear * 11.862615;
+  this.saturnYear   = this.earthYear * 29.447498;
+  this.uranusYear   = this.earthYear * 84.016846;
+  this.neptuneYear  = this.earthYear * 164.79132;
 }
 
 SpaceAge.prototype.onEarth = function() {
-  return Math.round((this.seconds / 31557600) * 100) / 100;
+  return calcAge(this.seconds, this.earthYear);
 }
 
 SpaceAge.prototype.onMercury = function() {
-  return Math.round((this.seconds / this.mercuryYear) * 100) / 100;
+  return calcAge(this.seconds, this.mercuryYear);
+}
+
+SpaceAge.prototype.onVenus = function() {
+  return calcAge(this.seconds, this.venusYear);
+}
+
+SpaceAge.prototype.onMars = function() {
+  return calcAge(this.seconds, this.marsYear);
+}
+
+SpaceAge.prototype.onJupiter = function() {
+  return calcAge(this.seconds, this.jupiterYear);
+}
+
+SpaceAge.prototype.onSaturn = function() {
+  return calcAge(this.seconds, this.saturnYear);
+}
+
+SpaceAge.prototype.onUranus = function() {
+  return calcAge(this.seconds, this.uranusYear);
+}
+
+SpaceAge.prototype.onNeptune = function() {
+  return calcAge(this.seconds, this.neptuneYear);
+}
+
+function calcAge(ageInSeconds, secondsInYear) {
+  return Math.round((ageInSeconds / secondsInYear) * 100) / 100;
 }
 
 module.exports = SpaceAge;
-
-/*
-
-   - Earth: orbital period 365.25 Earth days, or 31557600 seconds
-   - Mercury: orbital period 0.2408467 Earth years
-   - Venus: orbital period 0.61519726 Earth years
-   - Mars: orbital period 1.8808158 Earth years
-   - Jupiter: orbital period 11.862615 Earth years
-   - Saturn: orbital period 29.447498 Earth years
-   - Uranus: orbital period 84.016846 Earth years
-   - Neptune: orbital period 164.79132 Earth years
-
-*/
