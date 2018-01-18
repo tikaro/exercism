5b8bf7b93ccd9b29fc36bbccb796aa3579c694e1
John Young <john.young@thinkcompany.com>
2018-01-18 09:05:02 -0500

    Most tests passing.  Solution is extremely verbose

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -5,13 +5,27 @@ var Binary = function(input) {
 }
 
 Binary.prototype.toDecimal = function() {
-  var inputArray = this.input.split();
+  var inputArray = this.input.split('');
 
   var decimalValue = 0;
 
+  console.log(`-------- ${this.input} -----------`);
+  console.log(`inputArray.length is ${inputArray.length}`)
+
   for(var i=inputArray.length; i>0; i--) {
-    console.log(`Hi John: i is ${i}`);
-    decimalValue += inputArray[i-i] * 2^i;
+    console.log(`+++ i is ${i} +++`);
+
+    var currentBinaryValue = parseInt(inputArray[inputArray.length - i]);
+    console.log(`currentBinaryValue is inputArray[inputArray.length - i] -> inputArray[${inputArray.length} - ${i}] -> inputArray[${inputArray.length - i}] -> ${currentBinaryValue}`);
+
+    var binaryPlaceValue = 2**(i-1);
+    console.log(`binaryPlaceValue is 2**(i-1) -> 2**(${i}-1) -> 2**(${i-1}) -> ${binaryPlaceValue}`)
+
+    var valueToAdd = currentBinaryValue * binaryPlaceValue;
+    console.log(`valueToAdd is currentBinaryValue * binaryPlaceValue -> ${currentBinaryValue} * ${binaryPlaceValue} -> ${valueToAdd}`)
+
+    decimalValue += valueToAdd;
+    console.log(`decimalValue is now ${decimalValue}`);
   }
 
   return decimalValue;
