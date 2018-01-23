3ac92e933570dcf66c076f81e08a5d2b82e9ab61
John Young <john.young@thinkcompany.com>
2018-01-23 08:26:18 -0500

    Reverse the array to make it simpler to iterate over

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const DEBUG = false;
+
 var Binary = function(input) {
   this.input = input;
 }
@@ -9,14 +11,23 @@ Binary.prototype.toDecimal = function() {
    return 0;
  }
   
-  var inputArray = this.input.split('');
+  var inputArray = this.input.split('').reverse();
   var decimalValue = 0;
 
-  for(var i=inputArray.length; i>0; i--) {
-    var currentBinaryValue = parseInt(inputArray[inputArray.length - i]);
-    var binaryPlaceValue = 2**(i-1);
+  DEBUG && console.log(`++++++++++++++++ ${this.input} +++++++++++++++++`)
+
+  for(var i=0; i < inputArray.length; i++) {
+    var currentBinaryValue = parseInt(inputArray[i]);
+    DEBUG && console.log(`currentBinaryValue is ${currentBinaryValue}`);
+
+    var binaryPlaceValue = 2**(i);
+    DEBUG && console.log(`binaryPlaceValue is ${binaryPlaceValue}`);
+
     var valueToAdd = currentBinaryValue * binaryPlaceValue;
+    DEBUG && console.log(`valueToAdd is ${valueToAdd}`);
+
     decimalValue += valueToAdd;
+    DEBUG && console.log(`decimalValue is now ${decimalValue}`);
   }
 
   return decimalValue;
