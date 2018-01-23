1f45712364af152ae283ebf203bd71a516db6fe3
John Young <john.young@thinkcompany.com>
2018-01-23 08:10:31 -0500

    Return 0 if the input string contains anything besides 0 or 1

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const DEBUG = false;
-
 var Binary = function(input) {
   this.input = input;
 }
@@ -12,26 +10,13 @@ Binary.prototype.toDecimal = function() {
  }
   
   var inputArray = this.input.split('');
-
   var decimalValue = 0;
 
-  DEBUG && console.log(`-------- ${this.input} -----------`);
-  DEBUG && console.log(`inputArray.length is ${inputArray.length}`)
-
   for(var i=inputArray.length; i>0; i--) {
-    // console.log(`+++ i is ${i} +++`);
-
     var currentBinaryValue = parseInt(inputArray[inputArray.length - i]);
-    // console.log(`currentBinaryValue is inputArray[inputArray.length - i] -> inputArray[${inputArray.length} - ${i}] -> inputArray[${inputArray.length - i}] -> ${currentBinaryValue}`);
-
     var binaryPlaceValue = 2**(i-1);
-    // console.log(`binaryPlaceValue is 2**(i-1) -> 2**(${i}-1) -> 2**(${i-1}) -> ${binaryPlaceValue}`)
-
     var valueToAdd = currentBinaryValue * binaryPlaceValue;
-    // console.log(`valueToAdd is currentBinaryValue * binaryPlaceValue -> ${currentBinaryValue} * ${binaryPlaceValue} -> ${valueToAdd}`)
-
     decimalValue += valueToAdd;
-    // console.log(`decimalValue is now ${decimalValue}`);
   }
 
   return decimalValue;
