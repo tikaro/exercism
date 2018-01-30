6dc5bb0f41a3d912e3166c53d6caac200ffda94a
John Young <john.young@thinkcompany.com>
2018-01-30 17:40:29 -0500

    Update the Bob spec syntax to use Jest

--- javascript/bob/bob.spec.js
+++ javascript/bob/bob.spec.js
@@ -1,69 +1,69 @@
 var Bob = require('./bob.js');
 
-describe('Bob', function () {
+describe('Bob responds properly to...', function () {
   var bob = new Bob();
 
-  it('stating something', function () {
+  it('a statement', function () {
     var result = bob.hey('Tom-ay-to, tom-aaaah-to.');
     expect(result).toEqual('Whatever.');
   });
 
-  it('shouting', function () {
+  it('a shout', function () {
     var result = bob.hey('WATCH OUT!');
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  it('shouting gibberish', function () {
+  it('all-caps gibberish', function () {
     var result = bob.hey('FCECDFCAAB');
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  it('asking a question', function () {
+  it('a question', function () {
     var result = bob.hey('Does this cryogenic chamber make me look fat?');
     expect(result).toEqual('Sure.');
   });
 
-  it('asking a numeric question', function () {
+  it('a numeric question', function () {
     var result = bob.hey('You are, what, like 15?');
     expect(result).toEqual('Sure.');
   });
 
-  it('asking gibberish', function () {
+  it('a gibberish question', function () {
     var result = bob.hey('fffbbcbeab?');
     expect(result).toEqual('Sure.');
   });
 
-  it('talking forcefully', function () {
+  it('a forceful question', function () {
     var result = bob.hey('Let\'s go make out behind the gym!');
     expect(result).toEqual('Whatever.');
   });
 
-  it('using acronyms in regular speech', function () {
+  it('acronyms in regular speech', function () {
     var result = bob.hey('It\'s OK if you don\'t want to go to the DMV.');
     expect(result).toEqual('Whatever.');
   });
 
-  it('forceful questions', function () {
+  it('a forceful question', function () {
     var result = bob.hey('WHAT THE HELL WERE YOU THINKING?');
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  it('shouting numbers', function () {
+  it('forceful numbers', function () {
     var result = bob.hey('1, 2, 3 GO!');
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  it('only numbers', function () {
+  it('a question that is only numbers', function () {
     var result = bob.hey('1, 2, 3');
     expect(result).toEqual('Whatever.');
   });
 
-  it('question with only numbers', function () {
+  it('a question with only one number', function () {
     var result = bob.hey('4?');
     expect(result).toEqual('Sure.');
   });
 
-  it('shouting with special characters', function () {
+  it('a shouted question with gibberish', function () {
     var result = bob.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!');
     expect(result).toEqual('Whoa, chill out!');
   });
@@ -98,17 +98,17 @@ describe('Bob', function () {
     expect(result).toEqual('Fine. Be that way!');
   });
 
-  it('multiple line question', function () {
+  it('a multiple line question', function () {
     var result = bob.hey('\nDoes this cryogenic chamber make me look fat?\nno');
     expect(result).toEqual('Whatever.');
   });
 
-  it('starting with whitespace', function () {
+  it('a question starting with whitespace', function () {
     var result = bob.hey('         hmmmmmmm...');
     expect(result).toEqual('Whatever.');
   });
 
-  it('ending with whitespace', function () {
+  it('a question ending with whitespace', function () {
     var result = bob.hey('Okay if like my  spacebar  quite a bit?   ');
     expect(result).toEqual('Sure.');
   });
@@ -118,7 +118,7 @@ describe('Bob', function () {
     expect(result).toEqual('Fine. Be that way!');
   });
 
-  it('non-question ending with whitespace', function () {
+  it('a non-question ending with whitespace', function () {
     var result = bob.hey('This is a statement ending with whitespace      ');
     expect(result).toEqual('Whatever.');
   });
