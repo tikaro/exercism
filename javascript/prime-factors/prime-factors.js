42151674e950ac84ac396ed959071b44d4427518
John Young <john.young@thinkcompany.com>
2018-02-10 20:29:02 -0500

    Add cribbed solution

---
+++ javascript/prime-factors/prime-factors.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* eslint-disable max-statements */
+
+exports.for = function(n) {
+  var primeFactors = [];
+  var possibleFactor = 2;
+  var number = n;
+  while (possibleFactor * possibleFactor <= number) {
+    while (number % possibleFactor === 0) {
+      primeFactors.push(possibleFactor);
+      number /= possibleFactor;
+    }
+    possibleFactor += 1;
+  }
+  if(number > 1) { primeFactors.push(number) }
+  return primeFactors;
+}
