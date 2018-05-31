de6c14110e78f50e58206796e5303e2aaee38cca
John Young <john.young@thinkcompany.com>
2018-05-31 08:28:16 -0400

    Second pass through the 'Bob' exercise.

--- javascript/bob/bob-original.js
+++ javascript/bob/bob-original.js
@@ -4,17 +4,17 @@ var Bob = function() {
 }
 
 Bob.prototype.hey = function(input) {
-  input = input.replace(/[\s]+$/g, ''); //Eliminate whitespace
+  input = input.replace(/[\s]+$/g, '');
 
   var response = "Whatever.";
-  if(this.isAsking(input)) { response = "Sure." }
-  if(this.isShouting(input)) { response = "Whoa, chill out!" }
-  if(this.isSilence(input)) { response = "Fine. Be that way!" }
+  if(this.isAsking(input)) { response = "Sure."; }
+  if(this.isShouting(input)) { response = "Whoa, chill out!"; }
+  if(this.isSilence(input)) { response = "Fine. Be that way!"; }
   return response;
 }
 
 Bob.prototype.isAsking = function(input) {
-  return input[input.length-1] === '?';
+  return input[input.length - 1] === '?';
 }
 
 Bob.prototype.isShouting = function(input) {
