1277b6a511986c7cbab14e9336abcde053624dfd
John Young <john.young@thinkcompany.com>
2019-03-25 09:52:51 -0400

    Commit results of running 'exercism download --exercise=hello-world --track=javascript'

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,7 +1,14 @@
+//
+// This is a stub file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at hello-world.spec.js--that should give you some hints about what is
+// expected here.
+
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function() {
-  return 'Hello, World!';
+  var returnString = 'Hello, World!';
+  return returnString;
 };
 
 module.exports = HelloWorld;
