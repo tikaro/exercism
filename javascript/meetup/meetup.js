d773250ee857c6bdace650251d6ddc05e5cbc5b3
John Young <john.young@thinkcompany.com>
2019-04-08 08:37:55 -0400

    Partial solution in progress

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,4 +1,24 @@
 export const meetupDay = (year, month, dayOfWeek, which) => {
-  return new Date(year, month, 13)
+  // Start on the first day of the month
+  let testDate = new Date(year, month, 1)
+
+  // 
+  if ( weekdayMatches( testDate, dayOfWeek )) {
+    return testDate
+  }
+
+  addDay(testDate)
 }
 
+function weekdayMatches(date, expectedWeekday) {
+  let actualWeekday = daysOfTheWeek.indexOf(date.getDay())
+  return expectedWeekday == actualWeekday
+}
+
+function addDay(date) {
+  return date.setDate(date.getDate() + 1)
+}
+
+const daysOfTheWeek = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(' ')
+
+// const teenths = [13, 14, 15, 16, 17, 18, 19]
