afbef40c6a007d3e8afce19eedd37c42c339fb66
John Young <john.young@thinkcompany.com>
2019-04-24 17:37:29 -0400

    First version with passing tests

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,47 +1,53 @@
+import { arrayExpression } from "@babel/types";
+
 export const meetupDay = (year, month, dayOfWeek, which) => {
 
   // Create a hash of days:
   let days = {
-    0:    [],
-    1:   [],
+    0: [],
+    1: [],
     2: [],
-    3:  [],
-    4:    [],
-    5:  [],
-    6:    []
+    3: [],
+    4: [],
+    5: [],
+    6: []
   }
 
   // Start on the first day of the month
   let currentDate = new Date(year, month, 1)
 
+  //make a two-dimensional array of dates.
   do {
-    // Push the current date onto days[currentDate.getDay()]
-    // console.log( `currentDate is ${currentDate.toString()}`);
-    // console.log( `days[${currentDate.getDay()}].push(${currentDate.toString()});`)
     days[currentDate.getDay()].push(currentDate.getDate());
 
     currentDate.setDate(currentDate.getDate() + 1);
   }
   while (currentDate.getMonth() == month)
 
-console.log( days[0].toString() )
+  // get an array of just Thursdays, or just Fridays
+  var candidateDates = days[DAYNAMES.indexOf(dayOfWeek)]
 
-  // Scan through the month and add each day to the appropriate array.
-  // For each day of the month,
-    
-    // Advance the date by one day
-  
+  if( which == 'teenth' ) {
+    // return the first date over 12
+    candidateDates = candidateDates.filter(candidateDate => candidateDate > 12 )
+    return new Date(year, month, candidateDates[0]);
+  }
 
-  // if 'which' is '1st' then get date[indexOf('monday')][0]
-  // if 'which' is '2nd' then get date[indexOf('tuesday')][1]
-  // if 'which' is 'teenth' then scan the array of date[indexOf('wednesday')] and check for the first value greater than or equal to 13.
-  // if 'which' is 'last' then get date[(indexOf('thursdays')].last
+  if( which == 'last' ) {
+    // return the last weekdate
+    return new Date(year, month, candidateDates.pop());
+  }
 
-  // return that date
+  // if there's no 5th
+  if ( ORDINALS.indexOf(which) > (candidateDates.length - 1 )) {
+    console.log('uh-oh, too long')
+    throw new Error('');
+  }
 
-
-  return new Date(year, month, days[2][0]);
+  //return an ordinal date
+    const theDate = new Date(year, month, candidateDates[ORDINALS.indexOf(which)]);
+    return theDate
 }
 
-const daysOfTheWeek = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(' ')
-const teenths = [13, 14, 15, 16, 17, 18, 19]
+const ORDINALS = "1st 2nd 3rd 4th 5th".split(' ');
+const DAYNAMES = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(' ');
