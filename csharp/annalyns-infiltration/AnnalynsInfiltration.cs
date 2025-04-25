18562f3afb7c11d1aa5ac81e674360d9609ef91d
John Young <john@tikaro.net>
2025-04-25 01:06:52 +0000

    Add Annalyn's Infiltration exercise

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,14 @@
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+        knightIsAwake || archerIsAwake || prisonerIsAwake;
+    
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) => 
+        !archerIsAwake && prisonerIsAwake;
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) {
+        return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake);
+    }
+}
