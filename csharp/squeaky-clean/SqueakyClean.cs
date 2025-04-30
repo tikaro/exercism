80511002081189f6c4a3790e4885e6e2fb928dcd
John Young <john@tikaro.net>
2025-04-30 23:07:18 +0000

    Partial solution to squeakyClean

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -2,6 +2,7 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        identifier = identifier.Replace(" ","_");
+        return identifier;
     }
 }
