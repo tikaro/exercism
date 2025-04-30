c1087a5dc755d3fbeb115bd36d01d38d44969548
John Young <john@tikaro.net>
2025-04-30 22:56:09 +0000

    Add squeaky-clean challenge

---
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,7 @@
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+    }
+}
