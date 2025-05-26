11896c4a34a771614e18f37fbc4923c242458439
John Young <john@tikaro.net>
2025-05-26 18:00:27 +0000

    First version where all tests pass

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -1,22 +1,36 @@
 static class SavingsAccount
 {
+        private const float PenaltyInterestRate = 3.213f;
+        private const float GoodInterestRate = 0.5f;
+        private const float BetterInterestRate = 1.621f;
+        private const float BestInterestRate = 2.475f;
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        if (balance < 0) { return PenaltyInterestRate; }
+        else if (balance < 1000) { return GoodInterestRate; }
+        else if (balance < 5000) { return BetterInterestRate;}
+        else { return BestInterestRate; }
     }
 
     public static decimal Interest(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+        return balance * (decimal)InterestRate(balance) / 100;
     }
 
     public static decimal AnnualBalanceUpdate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        return balance + Interest(balance);
     }
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        if (balance >= targetBalance) { return 0; }
+        int years = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
     }
 }
