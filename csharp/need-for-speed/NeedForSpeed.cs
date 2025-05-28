9f3de96d78e4c53934cc4292a2db179e01789f3c
John Young <john@tikaro.net>
2025-05-28 23:58:06 +0000

    Second iteration

--- csharp/need-for-speed/NeedForSpeed.cs
+++ csharp/need-for-speed/NeedForSpeed.cs
@@ -1,50 +1,39 @@
 class RemoteControlCar
 {
+    private readonly int _speed;
+    private readonly int _batteryDrain;
     private int batteryLevel;
-    private int speed;
-    private int batteryDrain;
-    private int distanceDriven = 0;
+    private int _distanceDriven = 0;
+
     public RemoteControlCar(int speed, int batteryDrain)
     {
         this.batteryLevel = 100;
-        this.speed = speed;
-        this.batteryDrain = batteryDrain;
+        this._speed = speed;
+        this._batteryDrain = batteryDrain;
     }
 
-    public bool BatteryDrained()
-    {
-        return (batteryLevel < this.batteryDrain);
-    }
+    public bool BatteryDrained() => (batteryLevel < this._batteryDrain);
 
-    public int DistanceDriven()
-    {
-        return this.distanceDriven;
-    }
+    public int DistanceDriven() => this._distanceDriven;
 
     public void Drive()
     {
-        if (this.batteryLevel < this.batteryDrain)
+        if (this.batteryLevel < this._batteryDrain)
         {
             return;
         }
-        this.distanceDriven += this.speed;
-        this.batteryLevel -= this.batteryDrain;
+        this._distanceDriven += this._speed;
+        this.batteryLevel -= this._batteryDrain;
     }
 
-    public static RemoteControlCar Nitro()
-    {
-        return new RemoteControlCar(50, 4);
-    }
+    public static RemoteControlCar Nitro() => new RemoteControlCar(50, 4);
 }
 
 class RaceTrack
 {
-    private int length;
+    private int _length;
 
-    public RaceTrack(int distance)
-    {
-        this.length = distance;
-    }
+    public RaceTrack(int distance) => this._length = distance;
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
@@ -52,6 +41,6 @@ class RaceTrack
         {
             car.Drive();
         }
-        return car.DistanceDriven() >= this.length;
+        return car.DistanceDriven() >= this._length;
     }
 }
