e5ce451a0fde505bda893566b5c9e353f2eb90c7
John Young <john@tikaro.net>
2025-06-02 01:09:27 +0000

    Third solution to log-analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,21 +1,7 @@
 public static class LogAnalysis
 {
-    public static string SubstringAfter(this string input, string delimiter)
-    {
-        input.Substring(input.IndexOf(delimiter) + delimiter.Length);
-    } 
-
-    public static string SubstringBetween(this string input, string startDelimiter, string endDelimiter)
-    {
-        int startIndex = input.IndexOf(startDelimiter);
-        startIndex += startDelimiter.Length;
-
-        int endIndex = input.IndexOf(endDelimiter, startIndex);
-
-        return input.Substring(startIndex, endIndex - startIndex);
-    }
-
+    public static string SubstringAfter(this string input, string delimiter) => input.Split(delimiter)[1];
+    public static string SubstringBetween(this string input, string startDelimiter, string endDelimiter) => input.Split(startDelimiter)[1].Split(endDelimiter)[0];
     public static string Message(this string log) => log.SubstringAfter("]: ");
-
     public static string LogLevel(this string log) => log.SubstringBetween("[", "]:");
 }
