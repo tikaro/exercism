08f69874b0e30ea898930a2c63b5602a6afe747f
John Young <john@tikaro.net>
2025-06-02 00:42:48 +0000

    Adding log-analysis exercise

---
+++ csharp/log-analysis/LogAnalysis.cs
@@ -0,0 +1,10 @@
+public static class LogAnalysis 
+{
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+
+    // TODO: define the 'SubstringBetween()' extension method on the `string` type
+    
+    // TODO: define the 'Message()' extension method on the `string` type
+
+    // TODO: define the 'LogLevel()' extension method on the `string` type
+}
