9b8170d19edd70dbe9e49b2bfbe9c65baaa9013e
John Young <john@tikaro.net>
2025-06-02 01:04:37 +0000

    Second solution to log-analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,10 +1,21 @@
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string input, string delimiter)
+    {
+        input.Substring(input.IndexOf(delimiter) + delimiter.Length);
+    } 
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
+    public static string SubstringBetween(this string input, string startDelimiter, string endDelimiter)
+    {
+        int startIndex = input.IndexOf(startDelimiter);
+        startIndex += startDelimiter.Length;
 
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+        int endIndex = input.IndexOf(endDelimiter, startIndex);
+
+        return input.Substring(startIndex, endIndex - startIndex);
+    }
+
+    public static string Message(this string log) => log.SubstringAfter("]: ");
+
+    public static string LogLevel(this string log) => log.SubstringBetween("[", "]:");
 }
