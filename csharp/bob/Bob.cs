9395010a05c6d816e4865cd7286c7364dcff955c
John Young <john@tikaro.net>
2025-06-04 00:17:13 +0000

    Second solution to 'bob'

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -2,6 +2,30 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (IsSilence(statement))
+        {
+            return "Fine. Be that way!";
+        }
+
+        if (IsAsking(statement) && IsShouting(statement))
+        {
+            return "Calm down, I know what I'm doing!";
+        }
+
+        if (IsAsking(statement))
+        {
+            return "Sure.";
+        }
+
+        if (IsShouting(statement))
+        {
+            return "Whoa, chill out!";
+        }
+
+        return "Whatever.";
     }
+
+    private static bool IsShouting(string statement) => statement.Any(char.IsLetter) && !statement.Any(char.IsLower);
+    private static bool IsAsking(string statement) => statement.Trim().EndsWith("?");
+    private static bool IsSilence(string statement) => string.IsNullOrWhiteSpace(statement);
 }
