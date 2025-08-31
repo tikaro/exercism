38784f14215db196205edf014ba5082087c1d74c
John Young <john@tikaro.net>
2025-08-31 12:58:49 +0000

    Second soluttion for Tim from Marketing

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -2,7 +2,7 @@ static class Badge
 {
     public static string Print(int? id, string name, string? department)
     {
-        var dept = department?.ToUpper() ?? "OWNER";
-        return id.HasValue ? $"[{id}] - {name} - {dept}" : $"{name} - {dept}";
+        string dept = department?.ToUpper() ?? "OWNER";
+        return $"{(id.HasValue ? $"[{id}] - " : "")}{name} - {dept}";
     }
 }
