21d6d27a34859dda5eee30b23074e6d61063aaff
John Young <john@tikaro.net>
2025-12-12 00:40:48 +0000

    Solution to Lucian

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,10 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutesInOven) => ExpectedMinutesInOven() - minutesInOven;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int numLayers) => numLayers * 2;
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int numLayers, int minutesInOven) => PreparationTimeInMinutes(numLayers) + minutesInOven;
 }
