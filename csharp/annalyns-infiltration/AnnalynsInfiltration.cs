e483367e76ad8531ad612e014497ca3872e5bdc4
John Young <john@tikaro.net>
2025-12-23 18:55:21 +0000

    Solution to Annalyn's Infiltration

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,21 +2,28 @@ static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        return(!knightIsAwake);
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        return ( knightIsAwake || archerIsAwake || prisonerIsAwake );
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        return ( !archerIsAwake && prisonerIsAwake );
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if (petDogIsPresent)
+        {
+            return !archerIsAwake;
+        }
+        else
+        {
+            return (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
+        }
     }
 }
