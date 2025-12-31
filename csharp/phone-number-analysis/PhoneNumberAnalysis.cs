65967e0122290a7a6ce135f795b6bc90db045690
John Young <john@tikaro.net>
2025-12-31 14:30:22 +0000

    First solution to phone-number-analysis

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,11 +2,19 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        int dialingCode = int.Parse(phoneNumber.Substring(0, 3));
+        int prefix = int.Parse(phoneNumber.Substring(4, 3));
+        int lineNumber = int.Parse(phoneNumber.Substring(8, 4));
+
+        bool isNewYork = dialingCode == 212;
+        bool isFake = prefix == 555;
+        string localNumber = lineNumber.ToString("D4");
+
+        return (isNewYork, isFake, localNumber);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
