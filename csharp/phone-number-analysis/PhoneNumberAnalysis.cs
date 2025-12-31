a53dcb216cfd0b9ef1bb734d762463eb03292ee4
John Young <john@tikaro.net>
2025-12-31 14:34:24 +0000

    Improved the string parser based on community solutions

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,13 +2,12 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        int dialingCode = int.Parse(phoneNumber.Substring(0, 3));
-        int prefix = int.Parse(phoneNumber.Substring(4, 3));
-        int lineNumber = int.Parse(phoneNumber.Substring(8, 4));
+        string dialingCode = phoneNumber[0..3];
+        string prefix = phoneNumber[4..7];
+        string localNumber = phoneNumber[8..];
 
-        bool isNewYork = dialingCode == 212;
-        bool isFake = prefix == 555;
-        string localNumber = lineNumber.ToString("D4");
+        bool isNewYork = dialingCode == "212";
+        bool isFake = prefix == "555";
 
         return (isNewYork, isFake, localNumber);
     }
