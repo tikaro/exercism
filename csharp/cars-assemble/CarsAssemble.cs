06dade5c4898ba9c7d5f3e260cbc31aea818a506
John Young <john@tikaro.net>
2026-01-03 23:18:18 +0000

    Solution to cars-assemble

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -2,16 +2,27 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        return speed switch
+        {
+            0 => 0.0,
+            >= 1 and <= 4 => 1.0,
+            >= 5 and <= 8 => 0.9,
+            9 => 0.8,
+            10 => 0.77,
+            _ => throw new ArgumentOutOfRangeException(nameof(speed), "Speed must be between 0 and 10 inclusive.")
+        };
     }
     
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        const double baseProductionRate = 221.0;
+        double successRate = SuccessRate(speed);
+        return speed * baseProductionRate * successRate;
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        double productionRatePerHour = ProductionRatePerHour(speed);
+        return (int)productionRatePerHour/60;
     }
 }
